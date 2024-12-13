5c8f40e1f107b080ffbf5157b96574e4427704ad
Migalhaz <miguel.chagas614@gmail.com>
2024-12-13 09:06:14 -0300

    Update: Lobby controller fixing issues

--- Werewolves Of Millers Hollow/Assets/Scripts/Lobby/LobbyController.cs
+++ Werewolves Of Millers Hollow/Assets/Scripts/Lobby/LobbyController.cs
@@ -1,6 +1,5 @@
 using Photon.Pun;
 using Photon.Realtime;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +7,6 @@ namespace Game.Lobby
 {
     public class LobbyController : MonoBehaviourPunCallbacks
     {
-        [Header("Editor Only")]
-        [SerializeField] bool m_setOnAwake = true;
-        
-
         [Header("Canvas")]
         [SerializeField] GameObject m_lobbyCanvas;
         [SerializeField] GameObject m_createRoomCanvas;
@@ -22,38 +17,44 @@ namespace Game.Lobby
 
         [Header("Lobby Components")]
         [SerializeField] RoomEnter m_enterTab;
+        [SerializeField, Min(0)] float m_secondsToRefresh;
+        float m_refreshTime;
+        List<RoomInfo> m_currentRoomList = new();
 
         private void Awake()
         {
 #if UNITY_EDITOR
-            if (m_setOnAwake)
+            if (!PhotonNetwork.IsConnected)
             {
                 PhotonNetwork.NickName = "Dev Testing";
                 PhotonNetwork.ConnectUsingSettings();
             }
 #endif
             EnableSwitchCanvas();
-            EnableCanvas(m_defaultCanvas);
-            PhotonNetwork.JoinLobby();
+            EnableCanvas(m_defaultCanvas);      
         }
-
         public override void OnEnable()
         {
             base.OnEnable();
             MultiplayerObserver.OnEnteringRoom += DisableSwitchCanvas;
         }
-
         public override void OnDisable()
         {
             base.OnDisable();
             MultiplayerObserver.OnEnteringRoom -= DisableSwitchCanvas;
 
         }
+        private void Update()
+        {
+            if (Time.time >= m_refreshTime)
+            {
+                UpdateRoomList();
+            }
+        }
 
         public void EnableLobby() => EnableCanvas(m_lobbyCanvas);
         public void EnableCreateRoom() => EnableCanvas(m_createRoomCanvas);
         public void EnableEnterRoom() => EnableCanvas(m_enterRoomCanvas);
-
         void EnableCanvas(GameObject canvas)
         {
             if (!m_canSwitchCanvas) return;
@@ -61,26 +62,37 @@ namespace Game.Lobby
             m_createRoomCanvas.SetActive(m_createRoomCanvas == canvas);
             m_enterRoomCanvas.SetActive(m_enterRoomCanvas == canvas);
         }
-
         void EnableSwitchCanvas()
         {
             m_canSwitchCanvas = true;
         }
-
         void DisableSwitchCanvas()
         {
             m_canSwitchCanvas = false;
         }
 
+        
+        public void UpdateRoomList()
+        {
+            m_currentRoomList ??= new();
+            m_enterTab.UpdateRoomList(m_currentRoomList);
+            m_refreshTime = Time.time + m_secondsToRefresh;
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinLobby();
+        }
+
         public override void OnJoinedRoom()
         {
             Debug.Log("Entrou na sala!");
-            GameSceneManager.Instance.LoadGameHubScene();
+            GameSceneManager.GetInstance(false).LoadGameHubScene();
         }
 
         public override void OnRoomListUpdate(List<RoomInfo> roomList)
         {
-            m_enterTab.UpdateRoomList(roomList);
+            m_currentRoomList = roomList;
         }
     }
 }
